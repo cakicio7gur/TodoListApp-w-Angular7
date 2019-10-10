e0a750891a687d4b6ae19df2857256a5daebc0bc
Özgür <39501502+cakicio7gur@users.noreply.github.com>
2019-10-11 02:00:43 +0300

    Angular Project
    Todo List Project added.

---
+++ todoListWebApplication/src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatIconModule} from '@angular/material';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotFoundComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatCardModule,
+    DragDropModule,
+    FormsModule,
+    HttpClientModule,
+    MatIconModule,
+    MatSnackBarModule
+  ],
+  providers: [
+    {provide: "apiUrl", useValue:"https://api.limantech.com/todo"}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ todoListWebApplication/src/app/components/home/home.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { TodoService } from 'src/app/services/todo.service';
+import { MatSnackBar } from '@angular/material';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  data = {};
+
+  constructor(
+    private todoService: TodoService, 
+    private _snackBar: MatSnackBar
+  ) { }
+
+  ngOnInit() {
+    this.getAllTodos()
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+    this.updateTodo();
+  }
+
+  openSnackBar(message: string) {
+    this._snackBar.open(message, "Başarılı", {
+      duration: 2000,
+    });
+  }
+
+  addTodo(todo){
+    const obj ={todo:todo.value};
+    this.todoService.addTodo(obj)
+    .subscribe((res:any)=>{
+      this.openSnackBar(res.message);
+      this.getAllTodos();
+      todo.value="";
+    },(err)=>{
+      console.log(err)
+    })
+  }
+
+  getAllTodos(){
+    this.todoService.getAllTodos()
+    .subscribe((res)=>{
+      Object.keys(res).forEach((key)=>{
+        this.data[key]=res[key]
+      })
+    },(err)=>{
+      console.log(err)
+    })
+  }
+
+  updateTodo(){
+    this.todoService.updateTodo(this.data)
+    .subscribe((res)=>{
+     console.log(res);
+    },(err)=>{
+     console.log(err);
+    })
+  }
+
+  removeTodo(id){
+    if(confirm('Bu maddeyi silmek istediğinize emin misiniz?'))
+    {
+      this.todoService.removeTodo(id)
+      .subscribe((res)=>{
+        console.log(res);
+        this.getAllTodos();
+       },(err)=>{
+        console.log(err);
+       });
+    }
+  }
+
+}
